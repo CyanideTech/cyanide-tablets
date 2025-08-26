5e6994d38103fdeb9b4108fff9b0411cffd19443
CyanideTech <cyborg_Waffles@outlook.com>
2025-08-26 21:36:16 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+mkdir ~/Pictures
+
+sudo pacman -S waybar
+sudo pacman -S hyprpaper
+sudo pacman -S rofi
+sudo pacman -S fastfetch
+curl -sS https://starship.rs/install.sh | sh
+
+cp fastfetch ~/.config/ -f -r
+cp hypr ~/.config/ -f -r
+cp rofi ~/.config/ -f -r
+cp waybar ~/.config/ -f -r
+cp kitty ~/.config/ -f -r
+cp .bashrc ~/ -f -r
+cp wallpaper1.png ~/Pictures/ -f -r
+
